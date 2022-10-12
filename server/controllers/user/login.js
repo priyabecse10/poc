d9bf1b38af2d44df11cfeca4cf57b4e9c9e13d81
priyabecse10 <103168941+priyabecse10@users.noreply.github.com>
2022-10-12 14:01:00 +0530

    Create login.js

---
+++ server/controllers/user/login.js
@@ -0,0 +1,46 @@
+const user = require('../../models').user;
+const bcrypt=require('bcryptjs');
+const jwt=require("jsonwebtoken");
+const secretKey="Todo-Poc-CRUD-API";
+module.exports = {
+  signin(req,res){
+    const email=req.body.email;
+    const password=req.body.password;
+    return user
+    .findOne({
+        where:{
+            email:req.body.email
+        }
+    })
+    .then(user=>{
+        //
+        if(!user){
+            return res.status(404).send({message:"user not found."});
+        }
+        const passwordIsValid=bcrypt.compareSync(
+            req.body.password,
+            user.password
+        );
+        if(!passwordIsValid){
+            return res.status(401).send({
+                message:"Invalid pasword!"
+            });
+        }
+        else{
+            
+           let token=jwt.sign({email,password},secretKey,{expiresIn:'1d'});
+                
+                res.status(200).send({
+                    id:user.id,
+                    name:user.name,
+                    email:user.email,
+                    password:user.password,
+                    accesstoken:token,
+                    msg:"success"
+                });
+                                 
+           }
+         })
+
+    },
+};

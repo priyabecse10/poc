408c3e68467bd023107ae05cf5801137fed6ed49
priyabecse10 <103168941+priyabecse10@users.noreply.github.com>
2022-10-12 14:06:05 +0530

    Create book.private.route.js

---
+++ server/controllers/routes/book/book.private.route.js
@@ -0,0 +1,9 @@
+const bookcontroller=require('../../controllers/book/book');
+const authUser=require('./middleware');
+
+module.exports=(app)=>{
+app.post('/v1/book/create',authUser,bookcontroller.create);
+app.post('/v1/book/update/:id',authUser,bookcontroller.update);
+app.get('/v1/book/list',authUser,bookcontroller.list);
+app.delete('/v1/book/delete/:id',authUser,bookcontroller.destroy);
+};

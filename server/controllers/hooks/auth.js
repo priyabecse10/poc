215f17ce885534a85e8c879f726bf24da4675ff9
priyabecse10 <103168941+priyabecse10@users.noreply.github.com>
2022-10-14 13:43:07 +0530

    Update auth.js

--- server/controllers/hooks/auth.js
+++ server/controllers/hooks/auth.js
@@ -1,14 +1,27 @@
 const jwt = require("jsonwebtoken");
 const secretKey="Todo-Poc-CRUD-API";
-module.exports={
- verifyToken(req,res,next){
-    const authHeader=req.headers["authorization"];
-    const token=authHeader&&authHeader.split(" ")[1];
-    if(token==null) return res.send("error");
-    jwt.verify(token,secretKey,(err,user)=>{
-        if(err)return res.send("error");
-        req.user=user;
+
+const verifyToken=()=>{
+    fastify.decorateRequest('user'," ");
+    fastify.addHook('preHandler',async(req,res,next)=>{
+        const authHeader=req.headers["Authorization"];
+        const token=authHeader&&authHeader.split(" ")[1];
+        if(token==null) return res.send("error");
+        try{
+        jwt.verify(token,seretKey,(err,data)=>{
+            if(err)return res.send("error");
+            else{
+                req.data=data;
+                res.send("token verified");
+            }
+        })
+    }catch(error){
+        res.send("error occurred");
+    }
         next();
     })
-}
 };
+
+module.exports={
+    verifyToken
+}
